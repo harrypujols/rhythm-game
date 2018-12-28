504044787fc8edb002b9340955e7fc521de40945
Harry Pujols <harrypujols@me.com>
2018-12-28 12:36:24 -0400

    input interaction

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -2,6 +2,7 @@ extends Node
 
 var location = 0
 var track_speed = .005
+var is_ready = false
 onready var trailer = find_node('trailer')
 
 func _ready():
@@ -9,14 +10,21 @@ func _ready():
 	# Initialization here
 	pass
 
+func _input(event):
+	if Input.is_action_just_pressed('ui_accept') and is_ready:
+		print('note hit!')
+		is_ready = false
+	else:
+		print('wrong!')
+	
 func _process(delta):
 	trailer.set_unit_offset(location)
 	location += track_speed
 
 
 func _on_marker_body_area_shape_entered(area_id, area, area_shape, self_shape):
-	print('entered region')
+	is_ready = true
 
 
 func _on_marker_body_area_shape_exited(area_id, area, area_shape, self_shape):
-	print('left region')
+	is_ready = false
