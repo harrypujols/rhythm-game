d9e64d3ea681ca4f8416b4cc56374b7719cfa32f
Harry Pujols <harrypujols@me.com>
2019-01-07 13:25:21 -0400

    changed name of var

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -3,7 +3,7 @@ extends Node
 var location = 0
 var track_speed = .005
 var score = 0
-onready var trailer = find_node('trailer')
+onready var trail = find_node('trail')
 onready var score_label = find_node('score_label')
 onready var action_buttons = get_tree().get_nodes_in_group('action_buttons')
 
@@ -13,7 +13,7 @@ func _ready():
 		action_button.connect('score', self, 'on_action_button_score')
 
 func _process(delta):
-	trailer.set_unit_offset(location)
+	trail.set_unit_offset(location)
 	location += track_speed
 
 func on_action_button_score():
