09dd272733a04d5d853dd44521ab67f38b4854e1
Harry Pujols <harrypujols@me.com>
2019-01-07 11:58:06 -0400

    refined action button controls

--- scripts/action_button.gd
+++ scripts/action_button.gd
@@ -1,35 +1,37 @@
 extends Node2D
 
-const input_text = {
-	'a': 'a',
-	'b': 'b',
-	'y': 'y',
-	'x': 'x',
-	'square': '[',
-	'circle': 'o',
-	'triangle': 't',
-	'arrow_key': '^',
-	'key': 'k',
-	'delete': 'd',
-	'enter': '<',
-	'spacebar': 'u',
-	'esc': 'e'
+const action = {
+	'up': {
+		'degree': 0,
+		'input': 'ui_up',
+		'text': '^'
+	},
+	
+	'down': {
+		'degree': 180,
+		'input': 'ui_down',
+		'text': '^'
+	},
+	
+	'left': {
+		'degree': -90,
+		'input': 'ui_left',
+		'text': '^'
+	},
+	
+	'right': {
+		'degree': 90,
+		'input': 'ui_right',
+		'text': '^'
+	}
 }
 
-const arrow_direction = {
-	'up': 0,
-	'down': 180,
-	'left': -90,
-	'right': 90
-}
+export(String, 'up', 'down', 'left', 'right') var tag = 'left'
 
-export(int, 'up', 'down', 'left', 'right') var button_name
+func set_button_tags():
+	print(tag)
+	self.set_rotation_degrees(action[tag].degree)
+	$button_icon.set_text(action[tag].text)
 
 func _ready():
-	self.set_rotation_degrees(arrow_direction.right)
-	$button_icon.set_text(input_text.arrow_key)
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+	set_button_tags()
