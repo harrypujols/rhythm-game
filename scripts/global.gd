123955ed6837f57a784dd004d4b4d570c82d47f0
Harry Pujols <harrypujols@me.com>
2019-01-08 10:57:52 -0400

    setting up game over screen

---
+++ scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var final_score = {
+	total = 0,
+	scored = 0,
+	percentage = 0
+}
--- scripts/stage.gd
+++ scripts/stage.gd
@@ -3,19 +3,40 @@ extends Node
 var location = 0
 var track_speed = .005
 var score = 0
+var turn = 0
+var total_score
 onready var trail = find_node('trail')
 onready var score_label = find_node('score_label')
 onready var action_buttons = get_tree().get_nodes_in_group('action_buttons')
 
+func proccess_score():
+	var score_percentage = (float(score) / total_score) * 100
+	
+	if total_score - score > 10:
+		# game over
+		global.final_score.total = total_score
+		global.final_score.scored = score
+		global.final_score.percentage = score_percentage
+		
+	total_score = trail.get_children().size() + total_score
+
 func _ready():
 	score_label.text = String(score)
+	total_score = trail.get_children().size()
+
 	for action_button in action_buttons:
 		action_button.connect('score', self, 'on_action_button_score')
 
 func _process(delta):
 	trail.set_unit_offset(location)
 	location += track_speed
+	if trail.unit_offset == 1:
+		location = 0
+		turn += 1
+	
+	proccess_score()
 
+	
 func on_action_button_score():
 	score += 1
 	score_label.text = String(score)
