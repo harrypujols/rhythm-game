71ce2b874af5a374871e9fee464b69698206b655
Harry Pujols <harrypujols@me.com>
2019-01-08 20:21:30 -0400

    instansiating keys

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -6,9 +6,22 @@ var score = 0
 var turn = 0
 var fails = 3
 var total_score
+var get_action_button = load('res://interface/action_button.tscn')
+var action_button_list = ['down', 'left', 'right']
 onready var trail = find_node('trail')
 onready var score_label = find_node('score_label')
 onready var action_buttons = get_tree().get_nodes_in_group('action_buttons')
+
+func instance_action_buttons():
+	var position = Vector2()
+	var gap = 0
+	for tag in action_button_list:
+		var action_button = get_action_button.instance()
+		action_button.tag = tag
+		action_button.connect('score', self, 'on_action_button_score')
+		trail.add_child(action_button)
+		action_button.position.x = action_button.position.x + gap
+		gap += 80
 
 func proccess_score():
 	var score_percentage = (float(score) / total_score) * 100
@@ -24,11 +37,10 @@ func proccess_score():
 		total_score = trail.get_children().size() + total_score
 
 func _ready():
+	instance_action_buttons()
+	
 	score_label.text = String(score)
 	total_score = trail.get_children().size()
-
-	for action_button in action_buttons:
-		action_button.connect('score', self, 'on_action_button_score')
 
 func _process(delta):
 	trail.set_unit_offset(location)
