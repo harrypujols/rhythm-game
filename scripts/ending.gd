c1edadeebe3de780a0636815cf2d07516ef2affa
Harry Pujols <harrypujols@me.com>
2019-01-10 20:01:39 -0400

    attempting game save

--- scripts/ending.gd
+++ scripts/ending.gd
@@ -5,9 +5,21 @@ onready var percentage_label = find_node('percentage')
 var score_text = 'You completed {score} of {total} keys'
 var percentage_text = '{percentage}% of keys'
 var score = String(global.final_score.scored)
+var high_score = String(global.final_score.high_score)
 var total = String(global.final_score.total)
 var percentage = String(global.final_score.percentage)
 
+
+func get_high_score():
+	global.load_game()
+
+	if score > high_score:
+		high_score = score
+		global.save_game()
+	
+	print(high_score)
+
 func _ready():
+	get_high_score()
 	final_score.text = score_text.format({'score': score, 'total': total})
 	percentage_label.text = percentage_text.format({'percentage': percentage})
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,7 +1,37 @@
 extends Node
 
 var final_score = {
-	total = 0,
-	scored = 0,
-	percentage = 0
+	'total': 0,
+	'scored': 0,
+	'percentage': 0,
+	'high_score': 0
 }
+
+const path = 'user://save.filex'
+
+static func load_game():
+	var file = File.new()
+	if not file.file_exists(path):
+		#if there's not such file
+		save_game()
+		return
+		
+	file.open(path, file.READ)
+	var text = file.get_as_text()
+	file.close()
+	var result_json = JSON.parse(text)
+	if result_json.error == OK:
+		print('loading')
+		global.final_score = result_json.result
+		print(result_json.result)
+	else:
+    	print('Error: ', result_json.error)
+    	print('Error Line: ', result_json.error_line)
+    	print('Error String: ', result_json.error_string)
+
+static func save_game():
+	var file = File.new()
+	file.open(path, File.WRITE)
+	print('saving')
+	file.store_string(global.final_score)
+	file.close()
