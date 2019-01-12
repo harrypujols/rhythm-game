3e74bb8211e655ab7d937aeca4f80e6c0c81c51a
Harry Pujols <harrypujols@me.com>
2019-01-12 11:52:07 -0400

    color feedback has bug

--- scripts/action_button.gd
+++ scripts/action_button.gd
@@ -27,6 +27,9 @@ const action = {
 }
 
 var action_ready = false
+var scored = false
+var color_red = Color('#dc322f')
+var color_white = Color('#ffffff')
 signal score
 
 export(String, 'up', 'down', 'left', 'right') var tag = 'left'
@@ -41,11 +44,16 @@ func _ready():
 func _input(event):
 	if Input.is_action_just_pressed(action[tag].input):
 		if action_ready:
+			scored = true
 			emit_signal('score')
 			action_ready = false
 
 func _on_button_body_area_shape_entered(area_id, area, area_shape, self_shape):
+	print(area.name)
 	action_ready = true
 
 func _on_button_body_area_shape_exited(area_id, area, area_shape, self_shape):
 	action_ready = false
+	print('exited')
+	if not scored:
+		$button_icon.set('custom_colors/font_color', color_red)
